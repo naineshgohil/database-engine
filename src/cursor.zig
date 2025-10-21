e0954d44aa8512e08a8233bf3ebdb71a42fdc070
Naineshkumar  Gohil <nainesh@Naineshkumars-Mac-mini.local>
2025-10-21 07:19:03 -0400

    Added cursor component

---
+++ src/cursor.zig
@@ -0,0 +1,47 @@
+const std = @import("std");
+const mem = std.mem;
+const Pager = @import("pager.zig");
+const Row = @import("row.zig");
+const Table = @import("table.zig");
+
+// Cursor is a pointer to a specific position in the table
+//
+
+pub const Cursor = struct {
+    table: *Table,
+
+    // Which row number is the cursor pointing to
+    row_num: u32,
+
+    //
+    end_of_table: bool,
+
+    //
+    pub fn tableStart(table: *Table) Cursor {
+        return Cursor{
+            .table = table,
+            .row_num = 0,
+            .end_of_table = (table.num_rows == 0),
+        };
+    }
+
+    //
+    pub fn tableEnd(table: *Table) Cursor {
+        return Cursor{ .table = table, .row_num = table.num_rows, .end_of_table = true };
+    }
+
+    //
+    pub fn value(self: *Cursor) ![]u8 {
+        return try self.table.rowSlot(self.row_num);
+    }
+
+    pub fn advance(self: *Cursor) void {
+        if (!self.end_of_table) {
+            self.row_num += 1;
+        }
+
+        if (self.row_num >= self.table.num_rows) {
+            self.end_of_table = true;
+        }
+    }
+};
